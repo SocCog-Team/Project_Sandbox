478ef84c1ec4176b7687310ea131729b438857a5
Nocerino <rnocerino@MAC091.local>
2019-09-24 19:09:57 +0200

    "trialwisedata"

---
+++ roberta_n/tn_trialwiseDataStructure.m
@@ -0,0 +1,28 @@
+%%This is to structure things in a way that each row corresponds to trial
+%%number and the columns have X, Y coordinates or timepoints depending on
+%%which sub-structure you're looking in . 
+%%Input- xytime: Structure of gaze or touch tracker Log file
+%%trialnum_tracker: Array in which each row corresponds to which trial
+%%number this data point corresponds to 
+%%nrows_maintask: number of rows in the trial log file
+%%Example to use: [TrialWiseData]= tn_trialwiseDataStructure(TrialLogDataStructure.data,trialnum_tracker,nrows_maintask)
+
+function[TrialWiseData]= tn_trialwiseDataStructure(xytime,trialnum_tracker,nrows_maintask)
+    TrialWiseData=([]);
+    for trialnum= 1:nrows_maintask
+        trialnum_idx=find(trialnum_tracker==trialnum);
+        if trialnum~=0 && isempty(trialnum_idx)~=1
+            TrialWiseData.TrialNumber(trialnum,1)=trialnum;
+        
+            TrialWiseData.timepoints(trialnum,1:length(trialnum_idx))=xytime(trialnum_idx,1);
+            TrialWiseData.xCoordinates(trialnum,1:length(trialnum_idx))=xytime(trialnum_idx,2);
+            TrialWiseData.yCoordinates(trialnum,1:length(trialnum_idx))=xytime(trialnum_idx,3);
+
+        end
+    end
+    TrialWiseData.timepoints(TrialWiseData.timepoints==0) = NaN;   
+    TrialWiseData.xCoordinates(TrialWiseData.xCoordinates==0) = NaN;   
+    TrialWiseData.yCoordinates(TrialWiseData.yCoordinates==0) = NaN;   
+
+end
+
